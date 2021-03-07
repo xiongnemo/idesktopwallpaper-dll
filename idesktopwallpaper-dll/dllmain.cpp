8709ae88a82c69df8dd2378a6d5f800bfb7d6cf9
Nemo Xiong <xiongnemo@126.com>
2021-03-07 13:13:09 +0800

    make variable names readable

--- idesktopwallpaper-dll/dllmain.cpp
+++ idesktopwallpaper-dll/dllmain.cpp
@@ -6,20 +6,20 @@
 
 extern "C" {
     __declspec(dllexport)
-        void SetWallpaper(int id, const wchar_t* s)
+        void SetWallpaper(int desired_monitor_id, const wchar_t* path_to_img)
     {
         HRESULT  ad;
-        UINT count;
-        WCHAR* mid = new WCHAR[200];
-        WCHAR* temp = mid;
-        memset(mid, 0, 200 * 2);
+        UINT monitor_count;
+        WCHAR* monitor_device_path = new WCHAR[200];
+        WCHAR* temp = monitor_device_path;
+        memset(monitor_device_path, 0, 200 * 2);
         CoInitialize(NULL);
 
         IDesktopWallpaper* p;
         if (SUCCEEDED(CoCreateInstance(__uuidof(DesktopWallpaper), 0, CLSCTX_LOCAL_SERVER, __uuidof(IDesktopWallpaper), (void**)&p))) {
-            p->GetMonitorDevicePathCount(&count); //count: the numbers of monitor
-            p->GetMonitorDevicePathAt(id, &mid);  //0: the first monitor  1: the second monitor
-            ad = p->SetWallpaper(mid, s);
+            p->GetMonitorDevicePathCount(&monitor_count); // monitor_count: the numbers of monitor
+            p->GetMonitorDevicePathAt(desired_monitor_id, &monitor_device_path);  // 0: the first monitor  1: the second monitor
+            ad = p->SetWallpaper(monitor_device_path, path_to_img);
             p->Release();
         }
         delete[] temp;
